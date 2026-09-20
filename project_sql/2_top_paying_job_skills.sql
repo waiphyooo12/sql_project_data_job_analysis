15badf0336f2e5d455de079f186192f53b337f79
Wai Phyo Oo <waiphyooo1212002@gmail.com>
2026-09-20 23:54:23 +0700

    add query 2

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -1,5 +1,6 @@
 WITH top_paying_jobs AS(
 SELECT 
+  job_id,
   job_title_short, 
   job_country ,
   salary_year_avg , 
@@ -10,8 +11,46 @@ LEFT JOIN company_dim AS com_d
 ON fact.company_id = com_d.company_id
 WHERE
   job_country IN ('Thailand', 'Singapore') AND
-  salary_year_avg is not null
+  salary_year_avg is not null And
+  job_title_short like '%Data%' And 
+  job_title_short not like '%Senior%'
 --HAVING 
   --job_title_short like '%Data%'
-ORDER BY 3 DESC
-);
+ORDER BY 4 DESC
+)
+
+select 
+job_title_short, 
+  skills,
+  job_country ,
+  salary_year_avg , 
+  company_name
+from 
+  top_paying_jobs AS a
+INNER JOIN skills_job_dim as b
+on a.job_id = b.job_id
+INNEr Join skills_dim as c
+on b.skill_id = c.skill_id
+ORDER BY 4 DESC
+LIMIT 20 
+;
+
+--Checking CTE
+SELECT 
+  job_id,
+  job_title_short, 
+  job_country ,
+  salary_year_avg , 
+  name as company_name
+FROM
+  job_postings_fact AS fact
+LEFT JOIN company_dim AS com_d
+ON fact.company_id = com_d.company_id
+WHERE
+  job_country IN ('Thailand', 'Singapore') AND
+  salary_year_avg is not null And
+  job_title_short like '%Data%' And 
+  job_title_short not like '%Senior%'
+--HAVING 
+  --job_title_short like '%Data%'
+ORDER BY 4 DESC;
